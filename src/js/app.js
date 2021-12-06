0316e2220a09648f6ab9e078f958fc28379f0f27
Александр <westerovs@mail.ru>
2021-12-06 23:40:31 +0300

    added class App

--- src/js/app.js
+++ src/js/app.js
@@ -2,62 +2,50 @@ import { getNextDate, getCurrentDay, render, getIsToday } from './utils/utils.js
 import { templateRowPlants } from './view/view.js'
 import { plants } from './utils/const.js';
 
-const table = document.querySelector('.table')
-const tableBody = table.querySelector('.table__tbody')
-const currentDayField = table.querySelector('.table__current-day')
-currentDayField.innerHTML  = `${ getCurrentDay() }`
-
-localStorage.setItem('plants', JSON.stringify(plants))
-const localStPlants = JSON.parse(localStorage.getItem('plants'))
-
-const caption = document.querySelector('caption');
-let isStorageSupport = true;
-let lastDay = '';
-
-try {
-  lastDay = localStorage.getItem('adult');
-} catch (err) {
-  // console.log(2)
-  isStorageSupport = false;
-}
-
-tableBody.addEventListener('change', function(event) {
-  if (lastDay) {
-    // caption.value = lastDay;
-    caption.innerHTML = lastDay;
+// временно тут вся логика
+class App {
+  constructor() {
+    this.table = document.querySelector('.table')
+    this.tableBody = this.table.querySelector('.table__tbody')
+    this.tableBody.addEventListener('change', this.tableBodyHandler)
   }
   
-  const currentRow = event.target.closest('.table__row')
-  const currentName = currentRow.dataset.name
-  const nextDateField = currentRow.querySelector('.table__data--next')
-  const interval = +currentRow.querySelector('.table__data--interval input').value
+  init() {
+    this.renderRowPlants()
+  }
+  
+  tableBodyHandler = (event) => {
+      const currentRow = event.target.closest('.table__row')
+      const currentName = currentRow.dataset.name
+      const nextDateField = currentRow.querySelector('.table__data--next')
+      const interval = +currentRow.querySelector('.table__data--interval input').value
 
-  if (event.target.closest('.table__data--interval')) {
-    plants.find(item => {
-      if (item.nameEng === currentName) {
-        item.interval = interval
+      if (event.target.closest('.table__data--interval')) {
+        plants.find(item => {
+          if (item.nameEng === currentName) {
+            item.interval = interval
+          }
+        })
       }
+
+      const lastDate = currentRow.querySelector('.last-date').value
+      const nextDate = getNextDate(lastDate, interval)
+
+      nextDateField.innerHTML = nextDate.peopleFormat()
+  }
+  
+  renderRowPlants = () => {
+    plants.forEach(item => {
+      const nextDate = getNextDate(item.lastDate, item.interval)
+      item.nextDate = nextDate
+      item.dayWeek = nextDate.dayWeek
+      item.nextDayWeek = nextDate.nextDayWeek
+
+      getIsToday(nextDate.inputFormat)
+
+      render(this.tableBody, templateRowPlants(item, getIsToday(nextDate.inputFormat)))
     })
   }
-  
-  const lastDate = currentRow.querySelector('.last-date').value
-  const nextDate = getNextDate(lastDate, interval)
-  
-  
-  nextDateField.innerHTML = nextDate.peopleFormat()
-})
-
-
-const renderRowPlants = () => {
-  plants.forEach(item => {
-    const nextDate = getNextDate(item.lastDate, item.interval)
-    item.nextDate = nextDate
-    item.dayWeek = nextDate.dayWeek
-    item.nextDayWeek = nextDate.nextDayWeek
-    
-    getIsToday(nextDate.inputFormat)
-    
-    render(tableBody, templateRowPlants(item, getIsToday(nextDate.inputFormat)))
-  })
 }
-renderRowPlants()
+
+new App().init()
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,21 @@
 // точка входа
+
+// для local storage заготовка
+
+// const currentDayField = table.querySelector('.table__current-day')
+// currentDayField.innerHTML  = `${ getCurrentDay() }`
+// localStorage.setItem('plants', JSON.stringify(plants))
+// const localStPlants = JSON.parse(localStorage.getItem('plants'))
+// const caption = document.querySelector('caption');
+// let isStorageSupport = true;
+// let lastDay = '';
+// try {
+//   lastDay = localStorage.getItem('adult');
+// } catch (err) {
+//   // console.log(2)
+//   isStorageSupport = false;
+// }
+// if (lastDay) {
+//   // caption.value = lastDay;
+//   caption.innerHTML = lastDay;
+// }
